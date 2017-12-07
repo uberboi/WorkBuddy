bd0bf127c51251dfa1dca06e85889d3793c513fc
sthuang <sthuang@ucsc.edu>
2017-12-07 14:30:57 -0800

    json date parse function

--- app/src/main/java/cmps121/workbuddy/CalendarTab.java
+++ app/src/main/java/cmps121/workbuddy/CalendarTab.java
@@ -155,4 +155,21 @@ public class CalendarTab extends Fragment {
         return convertedDate;
     }
 
+    public Date jsonParse( String input ) throws java.text.ParseException {
+        SimpleDateFormat df = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ssz" );
+        if ( input.endsWith( "Z" ) ) {
+            input = input.substring( 0, input.length() - 1) + "GMT-00:00";
+        } else {
+            int inset = 6;
+
+            String s0 = input.substring( 0, input.length() - inset );
+            String s1 = input.substring( input.length() - inset, input.length() );
+
+            input = s0 + "GMT" + s1;
+        }
+
+        return df.parse( input );
+
+    }
+
 }
